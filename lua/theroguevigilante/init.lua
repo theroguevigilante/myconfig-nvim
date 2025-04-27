ee475c994806b9736bb63e06088d566793b4f57d
TheRogueVigilante <NONE>
2025-04-28 02:23:37 +0530

    Some more config

--- lua/theroguevigilante/init.lua
+++ lua/theroguevigilante/init.lua
@@ -2,7 +2,7 @@ require("theroguevigilante.set")
 require("theroguevigilante.remaps")
 require("theroguevigilante.lazy")
 require("theroguevigilante.diagnostics")
-vim.lsp.enable('luals') -- enabling lsp for lua
+vim.lsp.enable('lua_ls') -- enabling lsp for lua
 vim.lsp.enable('pyright') -- enabling lsp for python
 vim.lsp.enable('rust_analyzer') --enabling lsp for rust
 vim.lsp.enable('ts_ls') --enabling lsp for javsascript
