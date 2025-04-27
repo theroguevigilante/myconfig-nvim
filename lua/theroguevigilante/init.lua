6236ca2adeb363c6014e46d5358304c3fb1caca1
TheRogueVigilante <NONE>
2025-04-27 23:43:39 +0530

    The nails are in the coffin baby copilot integration all the important language servers, finally I can say neovim completes me. +++ Also, I am 100 percent sure that I would be chaging things ++++ +++ If you are going to use my neovim config this is the part from where you should be using anything after it is very much personal preference of my own+++

--- lua/theroguevigilante/init.lua
+++ lua/theroguevigilante/init.lua
@@ -2,7 +2,8 @@ require("theroguevigilante.set")
 require("theroguevigilante.remaps")
 require("theroguevigilante.lazy")
 require("theroguevigilante.diagnostics")
-vim.lsp.enable('luals') -- enabling esp for lua
-vim.lsp.enable('pyright') -- enabling esp for python
-vim.lsp.enable('rust_analyzer') --enabling esp for rust
-vim.lsp.enable('ts_ls') --enabling esp for javsascript
+vim.lsp.enable('luals') -- enabling lsp for lua
+vim.lsp.enable('pyright') -- enabling lsp for python
+vim.lsp.enable('rust_analyzer') --enabling lsp for rust
+vim.lsp.enable('ts_ls') --enabling lsp for javsascript
+vim.lsp.enable('emmet_language_server') -- enabling lsp for emmet_language_server
