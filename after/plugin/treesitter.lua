874d24ea0a9d6cbe30a8287aa1a1e79c27a98e73
theroguevigilante <naimish82@proton.me>
2025-04-27 06:31:24 +0530

    changes to lsp and treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "rust", "python", "javascript", "html", "css"},
+  ensure_installed = { "c", "lua", "rust", "python", "javascript", "html", "css", "bash"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
